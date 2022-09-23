1a8a4e5f9359497559ad3c6d727aa574266e606b
Arpan Pandey <arpan@hackersreboot.tech>
2022-09-23 19:03:28 +0530

    Adding Desc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,5 +7,6 @@
 </svelte:head>
 
 <main>
-    TBD
+    <h1>Hashpoll</h1>
+    <p>Hashpoll is a hashnode widget that enables seamless polls on any post. It is a lightweight tool that generates code for you to put in hashnode's widget field to create polls that just blend in with the rest of your content. You can use it directly from the command line or through the Web interface, and manage your poll as needed.</p>
 </main>
